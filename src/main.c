df1f4539625682c1ce245203e64f3b180479ef80
chadd williams <will4614@pacificu.edu>
2023-06-02 09:32:01 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
+/****************************************************************************
+ File name:  	main.c
+ Author:     	
+ Date:       	
+ Class:			 	
+ Assignment:	
+ Purpose:			
+ ****************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+/****************************************************************************
+ Function: 	 	main
+
+ Description: hello world!
+
+ Parameters:	argc - the number of command line arguments
+              argv - the actual command line arguments
+
+ Returned:	 	EXIT_SUCCESS
+ ****************************************************************************/
+int main(int argc, char *argv[])
 {
-  return 0;
+  printf("HELLO CS STUDENT!\n\n");
+  return EXIT_SUCCESS;
 }
