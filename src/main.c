ddc735f7828715827db7a4ae401597b5f22039a0
chadd williams <will4614@pacificu.edu>
2023-06-02 21:01:38 +0000

    update coding standards, fix build task

--- src/main.c
+++ src/main.c
@@ -1,26 +1,25 @@
 /****************************************************************************
  File name:  	main.c
- Author:     	
- Date:       	
- Class:			 	
- Assignment:	
- Purpose:			
+ Author:
+ Date:
+ Class:
+ Assignment:
+ Purpose:
  ****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
-/****************************************************************************
- Function: 	 	main
+ /****************************************************************************
+  Function: 	 	main
 
- Description: hello world!
+  Description: hello world!
 
- Parameters:	argc - the number of command line arguments
-              argv - the actual command line arguments
+  Parameters:	argc - the number of command line arguments
+               argv - the actual command line arguments
 
- Returned:	 	EXIT_SUCCESS
- ****************************************************************************/
-int main(int argc, char *argv[])
-{
-  printf("HELLO CS STUDENT!\n\n");
+  Returned:	 	EXIT_SUCCESS
+  ****************************************************************************/
+int main (int argc, char* argv[]) {
+  printf ("HELLO CS STUDENT!\n\n");
   return EXIT_SUCCESS;
 }
